9b4cbaf571a53b0327d259524998af3e6f5d19bb
Ade Surya Iskandar <ade.iskandar@yokke.co.id>
2024-01-05 14:12:43 +0700

    update const

--- src/core/constant.ts
+++ src/core/constant.ts
@@ -3,4 +3,4 @@ export const jwtConstants = {
   secret: 'b2447a8af9e5195eed39911e18bf94951f614ee8d8dbd8f5a063bcd8a0cb673fc78a83355bd20f0cdeece9866f56c3fffadd5e57a799ff741db9d5909de87f7f',
 };
 
-export const MAX_GET = 3;
+export const MAX_GET = 10;
